085fc4d8349feaf3b0bd41f6252e984d336e4c40
axlvazmo <axlvazmo@gmail.com>
2023-01-25 14:03:12 -0700

    Added project files and set up project structure

---
+++ Vector-Addition-Scalar/kernel.cu
@@ -0,0 +1,67 @@
+﻿/*
+* Assignment 6
+* Task: Multipy a large vector with a scalar and ad another large vector
+* Axel Vazquez Montano
+*/
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+
+#define N	500
+
+
+__global__ void kernelOpps(int *aHost, int *bHost, int *cHost, int scalar, int n) {
+	int i = threadIdx.x;
+	if (i < n) {
+		cHost[i] = (aHost[i] * scalar) + bHost[i];		//computing output
+	}
+}
+
+int main() {
+	
+	const int scalar = 2;
+	int aHost[N];	//defining values on host device
+	int bHost[N];
+	int cHost[N];
+
+	/*Assigning initial values to vectors on host*/
+	for (int i = 0; i < N; i++) {
+		aHost[i] = i;	//values on the vectors will be the value of their index
+		bHost[i] = i;
+		cHost[i] = 0;
+	}
+
+	/*Declaring pointers to cuda Device*/
+	int *aDev, *bDev, *cDev;
+
+	/*Allocating memory on cuda device*/
+	cudaMalloc(&aDev, N * sizeof(int));
+	cudaMalloc(&bDev, N * sizeof(int));
+	cudaMalloc(&cDev, N * sizeof(int));
+
+	/*Copying data from host to cuda device*/
+	cudaMemcpy(aDev, aHost, N * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(bDev, bHost, N * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(cDev, cHost, N * sizeof(int), cudaMemcpyHostToDevice);
+
+	/*Launching Kernel*/
+	kernelOpps <<<1, N>>> (aDev, bDev, cDev, scalar, N);
+
+	cudaDeviceSynchronize();	//blocking any opperations until cuda device is done computing
+
+	/*Copying data from cuda device to host*/
+	cudaMemcpy(cHost, cDev, N * sizeof(int), cudaMemcpyDeviceToHost);
+
+	/*Printing first 20 results*/
+	for (int i = 0; i < 5; ++i) {
+		printf("c[%d] = %i\n", i, cHost[i]);
+	}
+
+	/*Free cuda device memory*/
+	cudaFree(aDev);
+	cudaFree(bDev);
+	cudaFree(cDev);
+
+	return 0;
+}
---
+++ Vector-Addition/kernel.cu
@@ -0,0 +1,42 @@
+﻿/*
+* Assignment 4
+* Vector addition using cuda device
+* Axel Vazquez Montano
+*/
+
+#include <stdio.h>
+#define N	1024
+
+__global__ void vectorAddi(int *a, int *b, int *c, int n){
+	int i = threadIdx.x; //identifying each thread
+	if(i < n){	//making sure that we don't go over the needed threads
+		c[i] = a[i] + b[i];
+	}
+}
+
+int main(){
+	int *a, *b, *c;		//initializing vectors
+	cudaMallocManaged(&a, N * sizeof(int));	//allocationg memory in cuda device
+	cudaMallocManaged(&b, N * sizeof(int));
+	cudaMallocManaged(&c, N * sizeof(int));
+
+	for (int i = 0; i < N; ++i) {	//assigning values to vectors
+		a[i] = i;	//vector calue will be the same as index value
+		b[i] = i;
+		c[i] = 0;
+	}
+
+	vectorAddi <<<1, N>>> (a, b, c, N);		//specifying launch config for kernel <<<1 thread block, number of threads>>> **Note that the number of threads must equal number of elements in the vector to avoid delay in computation
+
+	cudaDeviceSynchronize();	//blocking any opperations until cuda device is done computing
+
+	for (int i = 0; i < 20; ++i) {
+		printf("c[%d] = %d\n", i, c[i]);
+	}
+
+	cudaFree(a); // free memory space of the vectors
+	cudaFree(b);
+	cudaFree(c);
+
+	return 0;
+}
